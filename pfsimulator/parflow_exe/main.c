fe7c39a7b49c86ba7dcebcb0e5807ae86dfa85aa
Ian Bertolacci <ian.bertolacci@gmail.com>
2019-07-18 10:37:34 -0700

    kokkos initialize and finalize

--- pfsimulator/parflow_exe/main.c
+++ pfsimulator/parflow_exe/main.c
@@ -64,9 +64,12 @@ using namespace SAMRAI;
 
 #include <string.h>
 #include <ctype.h>
+#include <Kokkos_Core.hpp>
 
 int main(int argc, char *argv [])
 {
+
+  Kokkos::initialize(argc, argv);
   FILE *file = NULL;
 
   FILE *log_file = NULL;
@@ -405,5 +408,6 @@ int main(int argc, char *argv [])
   tbox::SAMRAI_MPI::finalize();
 #endif
 
+  Kokkos::finalize();
   return 0;
 }
